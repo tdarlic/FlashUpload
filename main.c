a4dc967dadda33633983e605d5ac6733516aaa57
Tomislav Darlic <tomislav.darlic@gmail.com>
2018-05-20 22:05:24 +0200

    Adding writing last known good memory position to file...

--- main.c
+++ main.c
@@ -72,21 +72,22 @@ void set_blocking(int fd, int should_block) {
 
 int get_response(char * expected_response, int timeout, int port) {
     unsigned long timerec = time(NULL);
-    char rcvbuf[500];
-    int rcv_pointer = 0;
+    char rcvbuf[300];
     int answer = 0;
     int n = 0;
+    char timebuf[30];
     char buf[300];
-    memset(rcvbuf, 0, 500);
+    memset(rcvbuf, 0, 300);
     memset(buf, 0, 300);
     do {
         n = read(port, &buf, sizeof buf); // read up to 16 characters if ready to read
         if (strlen(buf) > 0) {
 
             strcat(rcvbuf, buf);
-            memset(buf, 0, 256);
+            memset(buf, 0, 300);
             if (strstr(rcvbuf, expected_response) != NULL) {
                 answer = 1;
+                clear_uart_bufer(port);
             }
         }
     } while ((answer == 0) || ((time(NULL) - timerec) > timeout));
@@ -131,6 +132,46 @@ bool file_exists(char * filename) {
     return false;
 }
 
+void print_uart_buffer() {
+
+}
+
+void clear_uart_bufer(int port) {
+    int n = 0;
+    char buf[300];
+    n = read(port, &buf, sizeof buf);
+}
+
+int write_last_address(int address) {
+    FILE *f;
+    f = fopen("lastaddr.txt", "w");
+    if (f == NULL) {
+        printf("Error opening file lastaddr.txt!\n");
+        return 0;
+    }
+    fprintf(f, "%i\n", address);
+    fclose(f);
+}
+
+int read_last_address() {
+    FILE *f;
+    char addrstr[30];
+    memset(addrstr, 0, 30);
+    size_t len = 0;
+    ssize_t read;
+    f = fopen("lastaddr.txt", "r");
+    if (f == NULL) {
+        printf("Error opening file lastaddr.txt!\n");
+        return 0;
+    }
+    read = fgets(addrstr, 28, f);
+    if (read == -1) {
+        return 0;
+    }
+    fclose(f);
+    return atoi(addrstr);
+}
+
 /*
  * 
  */
@@ -144,6 +185,12 @@ int main(int argc, char** argv) {
     FILE *bfile;
     int perc_complete = 0;
     float percfl = 0;
+    int firstaddr = 0;
+    int zeroaddrerr = 0;
+    int send_last_addr = 0;
+    char last_addr_ch[20];
+
+    memset(last_addr_ch, 0, 20);
 
     if (argc < 2) {
         printf("File name missing\n");
@@ -155,6 +202,14 @@ int main(int argc, char** argv) {
         fprintf(stderr, "error opening file '%s' mode 'r': %s\n",
                 argv[1], strerror(errno));
         exit(-1);
+    }
+
+    if (argc > 2) {
+        if (0 == strcmp(argv[2], "-a")) {
+            send_last_addr = read_last_address();
+        }
+    } else {
+        send_last_addr = 0;
     }
 
     printf("Opening file: %s\n", argv[1]);
@@ -170,44 +225,61 @@ int main(int argc, char** argv) {
         printf("error %d opening %s: %s", errno, portname, strerror(errno));
         return (EXIT_FAILURE);
     }
-
     set_interface_attribs(fd, B57600, 0); // set speed to 57600 bps, 8n1 (no parity)
     set_blocking(fd, 0); // set no blocking
 
-
-
     printf("Waiting 300 sec for the board to be ready.\n");
     // wait for the board to send ready string
-
+    clear_uart_bufer(fd);
     if (get_response(ready, 300, fd)) {
         write(fd, "sender_ready\n", 14); // send initial string
+        printf("Board ready - starting\n");
     } else {
         printf("No response from board!");
         return (EXIT_FAILURE);
     }
-
+    printf("First address:%i\n", send_last_addr);
+    sprintf(last_addr_ch, "%i\n", send_last_addr);
+    write(fd, last_addr_ch, strlen(last_addr_ch));
+    clear_uart_bufer(fd);
     address = get_address(2, fd);
-    do {                
+    do {
         memset(buf, 0, 300);
         fseek(bfile, address, SEEK_SET);
         rcnt = fread(buf, 1, 256, bfile);
         write(fd, buf, rcnt);
         percfl = (((float) address / (float) 8388608) * 100);
-        perc_complete = (int) percfl;         
-        printf("%d%% - %x\r", perc_complete, address); 
+        perc_complete = (int) percfl;
+        printf("%d%% - %x\r", perc_complete, address);
         fflush(stdout);
-        
+
         address = get_address(2, fd);
-        if ((address < old_address) && (address != -1)){
+        if ((address < old_address) && (address != -1)) {
+            // zero address received - possible error in timing, retry
+            if (address == 0) {
+                if (zeroaddrerr > 5) {
+                    printf("\nZero address received %i times, exiting.\n", zeroaddrerr);
+                    write_last_address(old_address);
+                    break;
+                }
+                printf("\nZero address received, trying again...\n");
+                zeroaddrerr++;
+                continue;
+            }
             printf("\nError - address %d is smaller than old address %d \n", address, old_address);
+            write_last_address(old_address);
             break;
         }
-        
+        // mark as first zero address received
+        if (address == 0) {
+            firstaddr = 1;
+        }
+
         old_address = address;
     } while (address > -1);
 
     printf("Last address: %d\n", address);
-    
+
     fclose(bfile);
 
     return (EXIT_SUCCESS);
